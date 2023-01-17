7e5b1f1c5af2fe367ef14399a242ae3dc0d9e96d
John <lc@pop-os.localdomain>
2023-01-16 22:04:01 -0600

    Add disconnect ids

--- streamer.go
+++ streamer.go
@@ -26,10 +26,6 @@ const (
 	BinanceSmartChain Network = 56
 )
 
-type streamMessage struct {
-  
-}
-
 type Streamer struct {
 	m sync.Mutex
 
@@ -37,10 +33,16 @@ type Streamer struct {
 	KeySecret string
 	Network   Network
 
+	active bool
+
 	subs []func(txb []byte, noticed, propagated time.Time)
 
-  msgChan chan []byte
+	queuedMessages []struct {
+		b   []byte
+		ret chan error
+	}
 
+	conn   net.Conn
 	ctx    context.Context
 	cancel context.CancelFunc
 }
@@ -64,143 +66,228 @@ func (s *Streamer) OnTransaction(f func(txb []byte, noticed, propagated time.Tim
 	s.m.Unlock()
 }
 
+func (s *Streamer) _send(msg []byte) error {
+  var idx = 0
+  for idx < len(msg) {
+    if n, err := s.conn.Write(msg[idx:]); err != nil {
+      return errors.Wrap(err, "error sending b")
+    } else {
+      idx += n
+    }
+  }
+  return nil
+}
+
+func (s *Streamer) send(msg []byte) error {
+	s.m.Lock()
+	if s.active {
+	  s.m.Unlock()
+		return errors.New("no active connection; haven't yet begun streaming")
+	} else {
+    if s.conn == nil {
+      var c = make(chan error)
+      s.queuedMessages = append(s.queuedMessages, struct {
+        b   []byte
+        ret chan error
+      }{
+        b:   msg,
+        ret: c,
+      })
+	    s.m.Unlock()
+      return <-c
+    } else {
+      err := s._send(msg)
+      s.m.Unlock()
+      return err
+    }
+  }
+}
+
+//Tell Mevlink to broadcast an RLP-encoded transaction.
+func (s *Streamer) EmitTransaction(txb []byte) error {
+  return errors.Wrap(s.send(append([]byte{EMIT_TRANSACTION}, txb...)), "error emitting tx")
+}
+
 func (s *Streamer) Stream() error {
 	s.ctx, s.cancel = context.WithCancel(context.Background())
+  s.active = true
+  defer func() {
+    s.active = false
+  }()
 	for {
-		if err := s._stream(); err != nil {
-      log.Println("mevlink streaming error: " + err.Error() + "; reconnecting in one second...")
-      time.Sleep(time.Second)
+		if disconnect_id, err := s._stream(); err != nil {
+			log.Println("mevlink streaming error: " + err.Error() + "; reconnecting in one second...")
+		} else if disconnect_id >= 0 {
+			switch disconnect_id {
+			case DISCONNECT_UNKNOWN_USER:
+				return errors.New("couldn't authenticate to the server; api key ID doesn't exist")
+			case DISCONNECT_BAD_MAC:
+				return errors.New("couldn't authenticate to the server; api key ID exists but password is incorrect")
+			case DISCONNECT_TOO_MANY_CONNECTIONS:
+				return errors.New("couldn't authenticate to the server; api key ID exists but password is incorrect")
+			case DISCONNECT_PROTOCOL_ERROR:
+				return errors.New("this client was disconnected for making a protocol error; is it the latest version/have you made modifications?")
+			case DISCONNECT_INTERNAL_SERVER_ERROR:
+				log.Println("disconnected from server for internal server error; attempting to reconnect")
+			default:
+				log.Println("got unknown disconnect id of ", disconnect_id)
+			}
 		} else {
-      return nil;
-    }
+			return nil
+		}
+		time.Sleep(time.Second)
 	}
 }
 
 func (s *Streamer) Stop() {
 	if s.cancel != nil {
 		s.cancel()
-		log.Println("stopped mevlink streamer")
 	}
 }
+
+const (
+	DISCONNECT_UNKNOWN_USER int = iota
+	DISCONNECT_TOO_MANY_CONNECTIONS
+	DISCONNECT_PROTOCOL_ERROR
+	DISCONNECT_INTERNAL_SERVER_ERROR
+	DISCONNECT_BAD_MAC
+)
 
 //Begins the streaming process and starts making callbacks to functions
 //provided in OnTransaction. Automatically attempts to reconnect. Errors if the
 //connection handshake fails despite a backoff.
-func (s *Streamer) _stream() error {
-  conn, mac, err := s.connect()
-  if err != nil {
-    return errors.Wrap(err, "error connecting outright")
-  }
+func (s *Streamer) _stream() (int, error) {
+	conn, mac, err := s.connect()
+	if err != nil {
+		return 0, errors.Wrap(err, "error connecting outright")
+	}
 
-  //Now we are authenticated and the server should start sending TRANSACTION messages.
-  for {
-    select {
-    case <-s.ctx.Done():
-      conn.Close()
-      return nil
-    case msg := <-s.msgChan:
-      if _, err := conn.Write(msg); err != nil {
-        return errors.Wrap(err, "couldn't write to connection")
-      }
-    default:
-    }
-    msg_id, err := readUntilFull(1, conn)
-    if err != nil {
-      return errors.Wrap(err, "error reading msg id byte")
-    }
-    switch msg_id[0] {
-    case TRANSACTION:
-      //Each transaction message includes three things: the encoded
-      //transaction, an 8 byte unix microsecond timestamp indicating when the
-      //transaction was sent, and another 8 byte unix microsecond timestamp
-      //indicating when mevlink first learned about the transaction. Both
-      //timestamps are big-endian encoded.
+	s.m.Lock()
+	s.conn = conn
+	for _, v := range s.queuedMessages {
+		if err := s._send(v.b); err != nil {
+			v.ret <- err
+		} else {
+			v.ret <- nil
+		}
+	}
+  s.queuedMessages = s.queuedMessages[0:0]
+	s.m.Unlock()
+	defer func() {
+		s.m.Lock()
+		s.conn.Close()
+		s.conn = nil
+		for _, v := range s.queuedMessages {
+			v.ret <- errors.New("connection closed")
+		}
+    s.queuedMessages = s.queuedMessages[0:0]
+		s.m.Unlock()
+	}()
 
-      //All transactions in the ethereumplex include a length specifier near the
-      //beginning. BSC uses legacy transactions and so are rlp encoded.
-      //See: https://ethereum.org/en/developers/docs/data-structures-and-encoding/rlp/
-      tx, err := readUntilFull(1, conn)
-      if err != nil {
-        return errors.Wrap(err, "error reading tx ln header")
-      }
-      var remaining int
+	//Now we are authenticated and the server should start sending TRANSACTION messages.
+	for {
+		select {
+		case <-s.ctx.Done():
+			return -1, nil
+		default:
+		}
+		msg_id, err := readUntilFull(1, conn)
+		if err != nil {
+			return -1, errors.Wrap(err, "error reading msg id byte")
+		}
+		switch msg_id[0] {
+		case TRANSACTION:
+			//Each transaction message includes three things: the encoded
+			//transaction, an 8 byte unix microsecond timestamp indicating when the
+			//transaction was sent, and another 8 byte unix microsecond timestamp
+			//indicating when mevlink first learned about the transaction. Both
+			//timestamps are big-endian encoded.
 
-      if tx[0] > 0x80 && tx[0] < 0xb8 {
-        remaining = int(tx[0] - 0x80)
-      } else if tx[0] > 0xb7 && tx[0] < 0xc0 {
-        var ln_of_ln = tx[0] - 0xb7
-        if i_b, err := readUntilFull(int(ln_of_ln), conn); err != nil {
-          return errors.Wrap(err, "error reading ln of ln")
-        } else {
-          tx = append(tx, i_b...)
-          remaining = int(binary.BigEndian.Uint64(append(make([]byte, 8-len(i_b)), i_b...)))
-        }
-      } else if tx[0] > 0xc0 && tx[0] < 0xf8 {
-        remaining = int(tx[0] - 0xc0)
-      } else if tx[0] > 0xf7 {
-        var ln_of_ln = tx[0] - 0xf7
-        if i_b, err := readUntilFull(int(ln_of_ln), conn); err != nil {
-          return errors.Wrap(err, "error reading ln of ln")
-        } else {
-          tx = append(tx, i_b...)
-          remaining = int(binary.BigEndian.Uint64(append(make([]byte, 8-len(i_b)), i_b...)))
-        }
-      } else {
-        log.Fatal("bad transaction data; got ln of ", tx[0])
-      }
+			//All transactions in the ethereumplex include a length specifier near the
+			//beginning. BSC uses legacy transactions and so are rlp encoded.
+			//See: https://ethereum.org/en/developers/docs/data-structures-and-encoding/rlp/
+			tx, err := readUntilFull(1, conn)
+			if err != nil {
+				return 0, errors.Wrap(err, "error reading tx ln header")
+			}
+			var remaining int
 
-      if txb, err := readUntilFull(remaining, conn); err != nil {
-        return errors.Wrap(err, "error reading tx bytes")
-      } else {
-        tx = append(tx, txb...)
-      }
+			if tx[0] > 0x80 && tx[0] < 0xb8 {
+				remaining = int(tx[0] - 0x80)
+			} else if tx[0] > 0xb7 && tx[0] < 0xc0 {
+				var ln_of_ln = tx[0] - 0xb7
+				if i_b, err := readUntilFull(int(ln_of_ln), conn); err != nil {
+					return -1, errors.Wrap(err, "error reading ln of ln")
+				} else {
+					tx = append(tx, i_b...)
+					remaining = int(binary.BigEndian.Uint64(append(make([]byte, 8-len(i_b)), i_b...)))
+				}
+			} else if tx[0] > 0xc0 && tx[0] < 0xf8 {
+				remaining = int(tx[0] - 0xc0)
+			} else if tx[0] > 0xf7 {
+				var ln_of_ln = tx[0] - 0xf7
+				if i_b, err := readUntilFull(int(ln_of_ln), conn); err != nil {
+					return -1, errors.Wrap(err, "error reading ln of ln")
+				} else {
+					tx = append(tx, i_b...)
+					remaining = int(binary.BigEndian.Uint64(append(make([]byte, 8-len(i_b)), i_b...)))
+				}
+			} else {
+				log.Fatal("bad transaction data; got ln of ", tx[0])
+			}
 
-      //Now that you have the transaction, you can either respond to it
-      //immediately or wait for timing information and MAC indicating it hasn't
-      //been tampered with and is recent enough to act upon.
+			if txb, err := readUntilFull(remaining, conn); err != nil {
+				return -1, errors.Wrap(err, "error reading tx bytes")
+			} else {
+				tx = append(tx, txb...)
+			}
 
-      //Next is the "propagated" timestamp. This is a unix microsecond
-      //timestamp of when the mevlink relay received and sent us the
-      //transaction.
-      propagated_b, err := readUntilFull(8, conn)
-      if err != nil {
-        return errors.Wrap(err, "error reading propagation bytes")
-      }
-      propagated := time.UnixMicro(int64(binary.BigEndian.Uint64(propagated_b)))
+			//Now that you have the transaction, you can either respond to it
+			//immediately or wait for timing information and MAC indicating it hasn't
+			//been tampered with and is recent enough to act upon.
 
-      //Finally, we have the "noticed" timestamp. This gives the earliest time
-      //mevlink heard of the transaction's existence from other peers on BSC.
-      //Note that this may not be the earliest time *any* mevlink relay has
-      //heard about the transaction at time of sending.
-      noticed_b, err := readUntilFull(8, conn)
-      if err != nil {
-        return errors.Wrap(err, "error reading noticed bytes")
-      }
-      noticed := time.UnixMicro(int64(binary.BigEndian.Uint64(noticed_b)))
+			//Next is the "propagated" timestamp. This is a unix microsecond
+			//timestamp of when the mevlink relay received and sent us the
+			//transaction.
+			propagated_b, err := readUntilFull(8, conn)
+			if err != nil {
+				return -1, errors.Wrap(err, "error reading propagation bytes")
+			}
+			propagated := time.UnixMicro(int64(binary.BigEndian.Uint64(propagated_b)))
 
-      //Finally, the mac
-      given_mac, err := readUntilFull(32, conn)
-      if err != nil {
-        return errors.Wrap(err, "error reading mac")
-      }
-      mac.Reset()
-      mac.Write(msg_id)
-      mac.Write(tx)
-      mac.Write(propagated_b)
-      mac.Write(noticed_b)
-      if bytes.Compare(given_mac, mac.Sum(nil)) != 0 {
-        return errors.New("mac was incorrect")
-      } else {
-        s.m.Lock()
-        for _, f := range s.subs {
-          f(tx, noticed, propagated)
-        }
-        s.m.Unlock()
-      }
-    default:
-      return errors.New("unknown message id")
-    }
-  }
+			//Finally, we have the "noticed" timestamp. This gives the earliest time
+			//mevlink heard of the transaction's existence from other peers on BSC.
+			//Note that this may not be the earliest time *any* mevlink relay has
+			//heard about the transaction at time of sending.
+			noticed_b, err := readUntilFull(8, conn)
+			if err != nil {
+				return -1, errors.Wrap(err, "error reading noticed bytes")
+			}
+			noticed := time.UnixMicro(int64(binary.BigEndian.Uint64(noticed_b)))
+
+			//Finally, the mac
+			given_mac, err := readUntilFull(32, conn)
+			if err != nil {
+				return -1, errors.Wrap(err, "error reading mac")
+			}
+			mac.Reset()
+			mac.Write(msg_id)
+			mac.Write(tx)
+			mac.Write(propagated_b)
+			mac.Write(noticed_b)
+			if bytes.Compare(given_mac, mac.Sum(nil)) != 0 {
+				return -1, errors.New("mac was incorrect")
+			} else {
+				s.m.Lock()
+				for _, f := range s.subs {
+					f(tx, noticed, propagated)
+				}
+				s.m.Unlock()
+			}
+		default:
+			return -1, errors.New("unknown message id")
+		}
+	}
 }
 
 func (s *Streamer) connect() (net.Conn, hash.Hash, error) {
@@ -301,6 +388,8 @@ const (
 	CHALLENGE
 	CHALLENGE_RESPONSE
 	TRANSACTION
+	DISCONNECT
+	EMIT_TRANSACTION
 )
 
 func readUntilFull(amount int, conn net.Conn) ([]byte, error) {
